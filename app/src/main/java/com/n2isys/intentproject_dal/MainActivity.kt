7e04b78f4571142fbf78f1af33b12c7884d3409c
Daljung <msfall@hanmail.net>
2023-10-22 15:17:12 +0900

    다른화면을 추가하고 연동하기후

--- app/src/main/java/com/n2isys/intentproject_dal/MainActivity.kt
+++ app/src/main/java/com/n2isys/intentproject_dal/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.n2isys.intentproject_dal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony.Mms.Intents
 import com.n2isys.intentproject_dal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,13 @@ class MainActivity : AppCompatActivity() {
         var viewbind = ActivityMainBinding.inflate(layoutInflater,null,false)
 
         setContentView(viewbind.root)
+
+        viewbind.moveOtherBtn.setOnClickListener {
+
+            //다른화면으로 이동
+            val myIntent = Intent(this,OtherActivity::class.java)
+            startActivity(myIntent)
+        }
 
     }
 }
--- app/src/main/java/com/n2isys/intentproject_dal/OtherActivity.kt
+++ app/src/main/java/com/n2isys/intentproject_dal/OtherActivity.kt
@@ -2,10 +2,14 @@ package com.n2isys.intentproject_dal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.n2isys.intentproject_dal.databinding.ActivityMainBinding
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_other)
+
+        var viewbind = ActivityMainBinding.inflate(layoutInflater,null,false)
+
+        setContentView(viewbind.root)
     }
 }
