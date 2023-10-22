8535a9d71c41781619678b89b57739ccbc6c8d66
Daljung <msfall@hanmail.net>
2023-10-22 17:32:20 +0900

    전달해주는 문구를 메세지 화면에서 사용

--- app/src/main/java/com/n2isys/intentproject_dal/MessageActivity.kt
+++ app/src/main/java/com/n2isys/intentproject_dal/MessageActivity.kt
@@ -2,15 +2,17 @@ package com.n2isys.intentproject_dal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.n2isys.intentproject_dal.databinding.ActivityMessageBinding
 import com.n2isys.intentproject_dal.databinding.ActivityOtherBinding
 
 class MessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        var viewbind3 = ActivityOtherBinding.inflate(layoutInflater,null,false)
-
+        var viewbind3 = ActivityMessageBinding.inflate(layoutInflater,null,false)
         setContentView(viewbind3.root)
 
+        val intentMessage = intent.getStringExtra("message")
+        viewbind3.receiveMessageTxt.text = intentMessage
     }
 }
